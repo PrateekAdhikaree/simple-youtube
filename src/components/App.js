a8bd897d5dfa2b673b78be5674bbeecdaf3c2e2b
PrateekAdhikaree <soumyapadhikaree@gmail.com>
2020-05-17 19:10:22 -0400

    Playing Video and Housekeeping

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null, defaultTerm: 'Kuala Bears' };
+
+  componentDidMount() {
+    this.onTermSubmit(this.state.defaultTerm);
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -13,14 +18,36 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar
+          onFormSubmit={this.onTermSubmit}
+          defaultTerm={this.state.defaultTerm}
+        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { term: '' };
+  state = { term: this.props.defaultTerm };
 
   onInputChange = (event) => {
     this.setState({ term: event.target.value });
